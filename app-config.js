72e31bc6016eece7552f369c5e3c27a38e82e41a
Fígaz <vmfp942@gmail.com>
2026-08-11 13:53:17 -0400

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,14 @@
+export const appConfig = {
+  organizationId: "gd-advocacia",
+  organizationName: "GD Advocacia",
+
+  // Conta temporária de demonstração.
+  bootstrapAdminEmail: "demo.gd.advocacia@example.com",
+
+  initialSeatLimit: 10,
+  nextSeatLimit: 20,
+  upgradePrice: "A definir",
+
+  // WhatsApp da CRIAAI 3D.
+  whatsappNumber: "5595991156877"
+};
---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAQoiEDLpILfrWZ0zw19Nrd4e1SnxsnuI",
+  authDomain: "gd-advocacia-sistema.firebaseapp.com",
+  projectId: "gd-advocacia-sistema",
+  storageBucket: "gd-advocacia-sistema.firebasestorage.app",
+  messagingSenderId: "121073004194",
+  appId: "1:121073004194:web:92c66712d916f73663b1fb"
+};
